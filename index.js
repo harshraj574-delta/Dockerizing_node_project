d337d38d3f304b05831d0c3a945016c7cba9dca1
harshraj574-delta <harsh@cyberdelta.com>
2025-03-21 18:25:36 +0530

    commit

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
 
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World' }); 
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log('Server is running on port 3000');
 });
